e8358e103d140b8ee7400d53a39c490529659c57
omar <omarsherif011011@gmail.com>
2025-10-11 15:06:21 +0300

    Repositry Pattern

---
+++ GymManagmentDAL/Repositries/Implementation/MemberRepositry.cs
@@ -0,0 +1,43 @@
+﻿using GymManagmentDAL.Data.Contexts;
+using GymManagmentDAL.Entities;
+using GymManagmentDAL.Repositries.abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagmentDAL.Repositries.Implementation
+{
+    internal class MemberRepositry : IMemberRepositry
+    {
+        private readonly GymDbContext _dbContext=new GymDbContext();
+        public int Add(Member member)
+        {
+            _dbContext.Members.Add(member);
+            return _dbContext.SaveChanges();
+        }
+
+        public int Delete(int id)
+        {
+            var member = _dbContext.Members.Find(id);
+            if (member is null) 
+                return 0;
+         
+
+            _dbContext.Members.Remove(member);
+            return _dbContext.SaveChanges();
+        }
+
+        public IEnumerable<Member> GetAllMembers()=>_dbContext.Members.ToList();
+
+        public Member? GetMemberById(int id) => _dbContext.Members.Find(id);
+        
+
+        public int Update(Member member)
+        {
+           _dbContext.Members.Update(member);
+            return _dbContext.SaveChanges();
+        }
+    }
+}
