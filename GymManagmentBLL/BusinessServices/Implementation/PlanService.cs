77d6524ca442a1ed540da0cc8882f9258c0b6953
omar <omarsherif011011@gmail.com>
2025-11-20 18:20:16 +0200

    Plan Service 2

--- GymManagmentBLL/BusinessServices/Implementation/PlanService.cs
+++ GymManagmentBLL/BusinessServices/Implementation/PlanService.cs
@@ -30,11 +30,11 @@ namespace GymManagmentBLL.BusinessServices.Implementation
 
             return Plans.Select(P => new PlanViewModel
             {
-                Id=P.id,
-                Name=P.Name,
-                Description=P.Description,
-                Price=P.Price,
-                isActive=P.IsActive
+                Id = P.id,
+                Name = P.Name,
+                Description = P.Description,
+                Price = P.Price,
+                isActive = P.IsActive,
                 DurationDays = P.DurationDays
             });
 
@@ -59,17 +59,65 @@ namespace GymManagmentBLL.BusinessServices.Implementation
 
         public PlanToUpdateViewModel? GetPlanToUpdate(int PlanId)
         {
-            throw new NotImplementedException();
+            var plan = _unintOfWork
+                .GetRepositry<Plan>()
+                .GetById(PlanId);
+
+            if (plan is null || plan.IsActive == false || HasActiveMemberShips(PlanId)) return null;
+
+            return new PlanToUpdateViewModel
+            {
+                Name = plan.Name,
+                Description = plan.Description,
+                Price = plan.Price,
+                DurationDays = plan.DurationDays
+            };
         }
 
         public bool ToggleStatus(int planId)
         {
-            throw new NotImplementedException();
+            var plan = _unintOfWork.GetRepositry<Plan>().GetById(planId);
+            if (plan is null || HasActiveMemberShips(planId))
+                return false;
+
+            plan.IsActive = plan.IsActive == true ? false : true;
+
+            plan.UpdatedAt = DateTime.Now;
+            _unintOfWork.GetRepositry<Plan>().Update(plan);
+
+            return _unintOfWork.SaveChanges() > 0;
+
         }
 
         public bool UpdatePlan(int PlanId, PlanToUpdateViewModel planToUpdate)
         {
-            throw new NotImplementedException();
+            var plan = _unintOfWork.GetRepositry<Plan>().GetById(PlanId);
+
+            if (plan is null || planToUpdate is null) return false;
+
+            (plan.Description, plan.DurationDays, plan.Price)
+                = (planToUpdate.Description, planToUpdate.DurationDays, planToUpdate.Price);
+
+            plan.UpdatedAt = DateTime.Now;
+            try
+            {
+                _unintOfWork.GetRepositry<Plan>().Update(plan);
+                return _unintOfWork.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+
+
+        private bool HasActiveMemberShips(int planId)
+        {
+            var activeMemberShips = _unintOfWork.GetRepositry<MemberShip>()
+                .GetAll(X => X.PlanId == planId && X.Status == "Active");
+            return activeMemberShips.Any();
+
         }
     }
 }
